addbc51a78e1008edf8b8cb5b3851c5a4037661b
amtorresn <am.torresn@uniandes.edu.co>
2019-04-23 22:23:08 +0000

    almost done

--- file.cpp
+++ file.cpp
@@ -15,9 +15,13 @@ void rk4(int n, double y0, double z0, double delta, double w, string filename);
 
 int main(){
     
- explicit_Euler(200, 1, 0, 0.1, 1, "exp_01.dat");
- explicit_Euler(200, 1, 0, 0.01, 1, "exp_001.dat");
- explicit_Euler(200, 1, 0, 1, 1, "exp_1.dat");
+    explicit_Euler(400, 1, 0, 0.1, 1, "exp_01.dat");
+    explicit_Euler(400, 1, 0, 0.01, 1, "exp_001.dat");
+    explicit_Euler(400, 1, 0, 1, 1, "exp_1.dat");
+    rk4(400, 1, 0, 0.1, 1, "rk_01.dat");
+    rk4(400, 1, 0, 0.01, 1, "rk_001.dat");
+    rk4(400, 1, 0, 1, 1, "rk_1.dat");
+    
  return 0;   
 }
 
@@ -33,7 +37,6 @@ void explicit_Euler(int n, double y0, double z0, double delta, double w, string 
     ofstream outfile;
     outfile.open(filename);
     
-//     cout << "explicit" << endl;
     while(x<30){
         z_n = z;
         z += delta*(-w*w*y);
@@ -44,6 +47,45 @@ void explicit_Euler(int n, double y0, double z0, double delta, double w, string 
     outfile.close();
 }
 
+
 void rk4(int n, double y0, double z0, double delta, double w, string filename){
+    
+    double y = y0; double x = 0; double z = z0;
+    double z_n = 0;
+    double f0_z = 0; double f0_y = 0;
+    double f1_z = 0; double f1_y = 0;
+    double f2_z = 0; double f2_y = 0;
+    double f3_z = 0; double f3_y = 0;
+    double fprom_z = 0; double fprom_y = 0;
+    
+    ofstream outfile;
+    outfile.open(filename);
+    
+    
+    while(x<30){
+        
+        z_n = z;
+        f0_z = delta*(-w*w*y);
+        f0_y = delta*z_n;
+        
+        f1_z = y + delta*(-w*w*f0_y)/2;
+        f1_y = delta*f0_z/2;
+            
+        f2_z = y + delta*(-w*w*f1_y)/2;
+        f2_y = delta*f1_z/2;
+        
+        f3_z = y + delta*(-w*w*f2_y);
+        f3_y = delta*f2_z;
+        
+        fprom_z = f0_z/6 + f1_z/3 + f2_z/3 + f3_z/6;
+        fprom_y = f0_y/6 + f1_y/3 + f2_y/3 + f3_y/6;
+        
+        z += delta*fprom_z;
+        y += delta*z;
+        x += delta;
+        outfile << x << " " << y << " " << z << endl;
+    }
+    outfile.close();
+    
     
 }
--- plot.py
+++ plot.py
@@ -10,16 +10,42 @@ data_1 = np.loadtxt("exp_1.dat")
 #print(data_01[:,0])
 #print(data_01[:,1])
 
-plt.figure(figsize=(14,4))
 
-plt.subplot(1,3,1)
-plt.title("0.1")
+plt.figure(figsize=(15,15))
+
+plt.subplot(3,3,1)
+plt.title("$y$ vs $x$, 0.1")
 plt.plot(data_01[:,0], data_01[:,1])
-plt.subplot(1,3,2)
-plt.title("0.01")
+plt.subplot(3,3,2)
+plt.title("$y$ vs $x$, 0.01")
 plt.plot(data_001[:,0], data_001[:,1])
-plt.subplot(1,3,3)
-plt.title("1")
+plt.subplot(3,3,3)
+plt.title("$y$ vs $x$, 1")
 plt.plot(data_1[:,0], data_1[:,1])
+
+
+
+plt.subplot(3,3,4)
+plt.title("$dy/dx$ vs $x$, 0.1")
+plt.plot(data_01[:,0], data_01[:,2])
+plt.subplot(3,3,5)
+plt.title("$dy/dx$ vs $x$, 0.01")
+plt.plot(data_001[:,0], data_001[:,2])
+plt.subplot(3,3,6)
+plt.title("$dy/dx$ vs $x$, 1")
+plt.plot(data_1[:,0], data_1[:,2])
+
+
+plt.subplot(3,3,7)
+plt.title("$dy/dx$ vs $y$, 0.1")
+plt.plot(data_01[:,1], data_01[:,2])
+plt.subplot(3,3,8)
+plt.title("$dy/dx$ vs $y$, 0.01")
+plt.plot(data_001[:,1], data_001[:,2])
+plt.subplot(3,3,9)
+plt.title("$dy/dx$ vs $y$, 1")
+plt.plot(data_1[:,1], data_1[:,2])
+
+
 
 plt.savefig("fig.png")
