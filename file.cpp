638a5f71999ada59a7b961f95500d69f1ab8c2ea
amtorresn <am.torresn@uniandes.edu.co>
2019-04-23 23:27:08 +0000

    finish

--- file.cpp
+++ file.cpp
@@ -52,6 +52,7 @@ void rk4(int n, double y0, double z0, double delta, double w, string filename){
     
     double y = y0; double x = 0; double z = z0;
     double z_n = 0;
+    double y_new = 0; double z_new = 0;
     double f0_z = 0; double f0_y = 0;
     double f1_z = 0; double f1_y = 0;
     double f2_z = 0; double f2_y = 0;
@@ -68,20 +69,29 @@ void rk4(int n, double y0, double z0, double delta, double w, string filename){
         f0_z = delta*(-w*w*y);
         f0_y = delta*z_n;
         
-        f1_z = y + delta*(-w*w*f0_y)/2;
-        f1_y = delta*f0_z/2;
-            
-        f2_z = y + delta*(-w*w*f1_y)/2;
-        f2_y = delta*f1_z/2;
+        y_new = y + f0_y/2;
+        z_new = z + f0_z/2;
         
-        f3_z = y + delta*(-w*w*f2_y);
-        f3_y = delta*f2_z;
+        f1_z = delta*(-w*w*y_new);
+        f1_y = delta*z_new;
+        
+        y_new = y + f1_y/2;
+        z_new = z + f1_z/2;
+            
+        f2_z = delta*(-w*w*y_new);
+        f2_y = delta*z_new;
+        
+        y_new = y + f2_y;
+        z_new = z + f2_z;
+        
+        f3_z = delta*(-w*w*y_new);
+        f3_y = delta*z_new;
         
         fprom_z = f0_z/6 + f1_z/3 + f2_z/3 + f3_z/6;
         fprom_y = f0_y/6 + f1_y/3 + f2_y/3 + f3_y/6;
         
-        z += delta*fprom_z;
-        y += delta*z_n;
+        z += fprom_z;
+        y += fprom_y;
         x += delta;
         outfile << x << " " << y << " " << z << endl;
     }
